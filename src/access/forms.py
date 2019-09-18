4e7d29fa39c213cfd6b9c86c9fefd6a29dd8f0b0
lai-la <westphal.laila@gmail.com>
2019-09-18 15:07:05 +0200

    wip: basic html registration page, placeholders added

--- src/access/forms.py
+++ src/access/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
@@ -10,6 +11,34 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': 'Username'
+        }
+        self.fields['first_name'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': 'First Name'
+        }
+        self.fields['last_name'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': 'Last Name'
+        }
+        self.fields['email'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': 'Email'
+        }
+        self.fields['password1'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': 'Password'
+        }
+        self.fields['password2'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        }
 
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
