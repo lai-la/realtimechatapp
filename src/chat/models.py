6e6129a63fa07bd57119b67debbfbe7ae31f0702
lai-la <westphal.laila@gmail.com>
2019-09-20 11:14:46 +0200

    added username to chat

--- src/chat/models.py
+++ src/chat/models.py
@@ -11,5 +11,5 @@ class Message(models.Model):
     def __str__(self):
         return self.author.username
 
-    def last_20_messages():
-        return Message.objects.order_by('-timestamp').all()[:20]
+ #   def last_20_messages():
+ #       return Message.objects.order_by('-timestamp').all()[:20]
